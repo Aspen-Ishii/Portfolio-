95c91bdc5043ddab3f53566c576e12793e65e9a3
Aspen Ishii <ishii.aspen@gmail.com>
2024-06-21 17:03:04 -0600

    Add ProjectsCards to reduce format redundancy

--- src/common/ProjectsCard.jsx
+++ src/common/ProjectsCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function ProjectCard({src }) {
+function ProjectCard({src, link, h3, p }) {
   return (
-    <a href="https://github.com/Aspen-Ishii/checklist" target="_blank">
-                <img className='hover' src={src} alt="Viberr logo" />
-                <h3>Viberr</h3>
-                <p>Streaming App</p>
+    <a href= {link} target="_blank">
+                <img className='hover' src={src} alt={`${h3} logo`} />
+                <h3>{h3}</h3>
+                <p>{p}</p>
             </a>
   )
 }
 
-export default ProjectCard
+export default ProjectCard;
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,5 +1,7 @@
 import styles from './ProjectsStyles.module.css';
 import viberr from '../../assets/viberr.png';
+import freshBurger from '../../assets/fresh-burger.png';
+import fitLift from '../../assets/fitlift.png'
 import ProjectsCard from '../../common/ProjectsCard';
 
 function Projects() {
@@ -7,7 +9,26 @@ function Projects() {
     <section id='Projects' className={styles.container}>
         <h1 className='sectionTile'>Projects</h1>
         <div className={styles.projectsContainer}>
-            <ProjectsCard src={viberr} />
+            {/*Note to self: reduce redundancy for future projects using an array
+             library or fetch from an API*/}
+            <ProjectsCard 
+            src={viberr}
+            link= "https://github.com/Aspen-Ishii/checklist"
+            h3= "Vibber"
+            p= "Streaming App"
+            />
+            <ProjectsCard 
+            src={freshBurger}
+            link= "https://github.com/Aspen-Ishii/checklist"
+            h3= "Fresh Burger"
+            p= "Hamburger Restaurant"
+            />
+            <ProjectsCard 
+            src={fitLift}
+            link= "https://github.com/Aspen-Ishii/checklist"
+            h3= "Fit Lift"
+            p= "Fitness App"
+            />
         </div>
     </section>
   );
