9b93ddf38d69eecf1550da044380223d0090d629
Aspen Ishii <ishii.aspen@gmail.com>
2024-07-08 16:20:47 -0600

    hook up Navbar and make it interactive

--- src/sections/Navbar/Nav.jsx
+++ src/sections/Navbar/Nav.jsx
@@ -1,4 +1,5 @@
-import Nav from './NavStyles.module.css'
+/*
+import styles from './NavStyles.module.css'
 
 function Nav() {
   return (
@@ -9,6 +10,38 @@ function Nav() {
             <li>Skills</li>
             <li>Contact</li>
           </ul>
+    </section>
+  )
+}
+
+export default Nav;
+*/
+
+import styles from './NavStyles.module.css'
+
+function Nav() {
+  return (
+    <section id='nav' className={styles.container}>
+      <nav>
+        <ul> 
+        <li>
+            <a href='#'>Aspen Ishii</a>
+          </li>
+          <li>
+            <a href='#hero'>About me</a>
+          </li>
+          <li>
+            <a href='#Projects'>Projects</a>
+          </li>
+          <li>
+            <a href='#skills'>Skills</a>
+          </li>
+          <li>
+            <a href='#contact'>Contact</a> 
+          </li>
+        </ul>
+        </nav>
+      
     </section>
   )
 }
