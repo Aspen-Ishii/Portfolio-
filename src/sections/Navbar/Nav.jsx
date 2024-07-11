4362048175a967b118dca0a3d6fdc16c1d983715
Aspen Ishii <ishii.aspen@gmail.com>
2024-07-11 16:33:51 -0600

    Add animation and outside-click-close feature to side Nav bar

--- src/sections/Navbar/Nav.jsx
+++ src/sections/Navbar/Nav.jsx
@@ -12,7 +12,8 @@ function Nav() {
     <section id='nav' className={styles.container}>
       <nav>
         <ul className={`${styles.sidebar} ${sidebarVisible ? styles.sidebarVisible : ''}`}> 
-        <li className={styles.closeSidebar} onClick={toggleSidebar}>
+        
+          <li className={styles.closeSidebar} onClick={toggleSidebar} >
             <a><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a>
           </li>
           <li>
@@ -47,10 +48,12 @@ function Nav() {
           </li>
           <li className={styles.menuBTN} onClick={toggleSidebar}>
             <a href='#'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a> 
+            
           </li>
         </ul>
+        {sidebarVisible && <div className={styles.overlay} onClick={toggleSidebar}></div>}
       </nav>
-
+      
     </section>
   )
 }
